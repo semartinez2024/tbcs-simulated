24cee5fbed234b1e9be0ee537563b8e70afc8897
semartinez2024 <r12853008@ntu.edu.tw>
2025-03-03 14:50:55 +0800

    reading data and checking variables

--- tbcs-simulated.R
+++ tbcs-simulated.R
@@ -1,3 +1,6 @@
 library(tidyverse)
 library(readxl)
 tbcs_simulated_data <- read_excel("tbcs_simulated_data.xlsx")
+
+# use pipe for readability of code
+# use rename, easier way to change column names
