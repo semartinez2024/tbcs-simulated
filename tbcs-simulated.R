45b197c5c6b0319594c97be63ad0b5597fbab060
semartinez2024 <r12853008@ntu.edu.tw>
2025-03-03 14:52:40 +0800

    reading data and checking variables

--- tbcs-simulated.R
+++ tbcs-simulated.R
@@ -4,3 +4,12 @@ tbcs_simulated_data <- read_excel("tbcs_simulated_data.xlsx")
 
 # use pipe for readability of code
 # use rename, easier way to change column names
+
+head(tbcs_simulated_data)
+tail(tbcs_simulated_data)
+nrow(tbcs_simulated_data)
+ncol(tbcs_simulated_data)
+glimpse(tbcs_simulated_data)
+str(tbcs_simulated_data)
+
+#rename variables and run glimpse again and continue onto Part 4
