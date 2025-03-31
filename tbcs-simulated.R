68eeaf08b2e3d96b43a58d167055cb43c84a5691
semartinez2024 <r12853008@ntu.edu.tw>
2025-03-31 14:23:38 +0800

    remove getwd and setwd, view column and row numbers for codebook

--- tbcs-simulated.R
+++ tbcs-simulated.R
@@ -1,15 +1,46 @@
 library(tidyverse)
 library(readxl)
-tbcs_simulated_data <- read_excel("tbcs_simulated_data.xlsx")
+library(dplyr)
 
-# use pipe for readability of code
-# use rename, easier way to change column names
+tbcs_simulated_data <- read_excel("tbcs_simulated_data.xlsx")
+View(tbcs_simulated_data)
+
+#use pipe for readability of code
+#use rename, easier way to change column names
 
 head(tbcs_simulated_data)
 tail(tbcs_simulated_data)
 nrow(tbcs_simulated_data)
 ncol(tbcs_simulated_data)
-glimpse(tbcs_simulated_data)
-str(tbcs_simulated_data)
+glimpse(tbcs_simulated_data) #variable type, variable numbers, and observations
+str(tbcs_simulated_data) #variable type, variable numbers, and observations
+
+tbcs6mo_codebook_chinese <- read_excel("tbcs-simulated/TBCS主問卷編碼簿(6個月大) CHN.xls")
+view(tbcs6mo_codebook_chinese)
+nrow(tbcs6mo_codebook_chinese) #149
+ncol(tbcs6mo_codebook_chinese) #6
+
+tbcs6mo_codebook_english <- read_excel("tbcs-simulated/TBCS主問卷編碼簿(6個月大) ENG.xlsx")
+view(tbcs6mo_codebook_english)
+nrow(tbcs6mo_codebook_english) #149
+ncol(tbcs6mo_codebook_english) #6
+
+tbcs18mo_codebook_chinese <- read_excel("tbcs-simulated/TBCS主問卷編碼簿(18個月大) CHN.xls")
+view(tbcs18mo_codebook_chinese)
+nrow(tbcs18mo_codebook_chinese) #24
+ncol(tbcs18mo_codebook_chinese) #5
+
+tbcs18mo_codebook_english <- read_excel("tbcs-simulated/TBCS主問卷編碼簿(18個月大) ENG.xlsx")
+view(tbcs18mo_codebook_english)
+nrow(tbcs18mo_codebook_english) #24
+ncol(tbcs18mo_codebook_english) #5
+
 
 #rename variables and run glimpse again and continue onto Part 4
+# base R
+## colnames(df) <- c("NewName1", "NewName2")
+# tidyverse
+
+#library(dplyr)
+##df <- df %>% rename(NewName1 = OldName1, NewName2 = OldName2)
+
