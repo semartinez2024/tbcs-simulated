0ac2bde8580d6c2046c38d5c9272f4f53bcac42f
semartinez2024 <r12853008@ntu.edu.tw>
2025-07-29 17:54:02 +0800

    column names changed

--- tbcs-simulated.R
+++ tbcs-simulated.R
@@ -59,35 +59,64 @@ tbcs_simulated_data <- rename(tbcs_simulated_data, #error in code saying that 'r
        participant_sex = b_sex_06,
        gestational_age = bb1_06m,
        birth_weight = bb2_06m,
-       singleton_or_twin = bb3_06m)
-
-colnames(tbcs_simulated_data) <- c("participant_identificaiton", "taiwanese_year_of_birth", "participant_sex")
+       singleton_or_twin = bb3_06m,
+       duration_drinking_breastmilk_or_formula = c2am_06m,	
+       breastfeeding_only = c2ad_06m,
+       breastmilk_as_main_source_months = c2bm_06m,	
+       breastmilk_as_main_source_days = c2bd_06m,	
+       formula_as_main_source_months = c2cm_06m,
+       formula_as_main_source_days = c2cd_06m,	
+       formula_only_months = c2dm_06m,	
+       formula_only_days = c2dd_06m,	
+       address = f5a1_06m,	
+       parent_smoking_status = h1aa_6m,	
+       daily_number_cigarettes = h1aa1_6m,	
+       father_smoking_status_before_pregnancy = h1ba_6m,	
+       father_daily_number_cigarettes_before_pregnancy = h1ba1_6m,	
+       mother_smoking_status_first_trimester = h1ab_6m,	
+       mother_daily_number_of_cigarettes_first_trimester = h1ab1_6m,	
+       father_smoking_status_first_trimester = h1bb_6m,	
+       father_daily_number_of_cigarettes_first_trimester = h1bb1_6m,	
+       mother_smoking_status_second_trimester = h1ac_6m,	
+       mother_daily_number_of_cigarettes_second_trimester = h1ac1_6m,	
+       father_smoking_status_second_trimester = h1bc_6m,	
+       father_daily_number_of_cigarettes_second_trimester = h1bc1_6m,	
+       mother_smoking_status = h1ad_6m,	
+       mother_daily_number_cigarettes = h1ad1_6m,	
+       father_smoking_status = h1bd_6m,	
+       father_daily_number_cigarettes = h1bd1_6m,	
+       mother_alcohol_consumption_before_pregnancy = h2_a_6m,	
+       mother_alcohol_consumption_before_pregnancy_over_3x_per_week = h2a_a_6m,	
+       mother_alcohol_consumption_during_pregnancy = h2_b_6m,	
+       mother_alcohol_consumption_during_pregnancy_over_3x_per_week = h2a_b_6m,	
+       mother_alcohol_consumption = h2_c_6m,	
+       mother_alcohol_consumption_over_3x_per_week = h2a_c_6m,	
+       father_alcohol_consumption = h2_d_6m,	
+       father_alcohol_consumption_over_3x_per_week = h2a_d_6m,	
+       average_monthly_income_past_year = h13_06m,	
+       incense_burning_at_home = k3_06m,	
+      # m_age_bi codebook has Mage but idk what bi is supposed to indicate	
+      # m_edu_g3 codebook has Medu but idkk what g3 is supposed to indicate
+      # don't need father's age or edu?? (check lit)
+       milestone_achievement = a6_1_18m,	
+       month_age_of_milestone_achievement = a6a_1_18m,	
+       milestone_walk_stedily = a6_2_18m,	
+       month_age_milestone_walk_steadily = a6a_2_18m,	
+       milestone_clapping = a6_3_18m,	
+       month_age_milestone_clapping = a6a_3_18m,	
+       milestone_scribble_with_pen = a6_4_18m,	
+       month_age_milestone_scribble_with_pen = a6a_4_18m,	
+       milestone_wave_goodbye = a6_5_18m,	
+       month_age_milestone_wave_goodbye = a6a_5_18m,	
+       milestone_call_a_parent = a6_6_18m,	
+       month_age_milestone_call_a_parent = a6a_6_18m,	
+       milestone_will_come_when_called = a6_7_18m,	
+       month_age_milestone_will_come_when_called = a6a_7_18m,	
+       milestone_drink_from_cup_with_both_hands = a6_8_18m,	
+       month_age_milestone_drink_from_cup_with_both_hands = a6a_8_18m)
 view(tbcs_simulated_data)
-names(tbcs_simulated_data) #column names
-                                                      c2am_06m
-                                                      c2ad_06m
-                                                      c2bm_06m
-                                                      c2bd_06m
-                                                      c2cm_06m
-                                                      c2cd_06m
-                                                      c2dm_06m
-                                                      c2dd_06m
-                                                      f5a1_06m
-                                                      h1aa_6m
-                                                      h1aa1_6m
-                                                      h1ba_6m
-                                                      h1ba1_6m
-                                                      h1ab_6m
-                                                      h1ab1_6m
-                                                      h1bb_6m
-                                                      h1bb1_6m
-                                                      h1ac_6m
-                                                      h1ac1_6m
-                                                      h1bc_6m
-                                                      h1bc1_6m
-                                                      h1ad_6m
-                                                      h1ad1_6m)
-##rename cols and change levels
+
+#don't need father's age or edu?? (check lit)
+## change levels
 
 #rename variables and run glimpse again and continue onto Part 4
-
