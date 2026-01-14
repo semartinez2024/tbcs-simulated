28ad603180316d45143ff93e60405c1849920243
semartinez2024 <r12853008@ntu.edu.tw>
2026-01-13 20:02:14 -0500

    r script using source to pull r scripts related to tbcs x probiotic consumption

---
+++ master_script.R
@@ -0,0 +1,5 @@
+source("C:/Users/msara/OneDrive/Documents/NTU GHP/Thesis/Data/Outcome_tbcs-simulated/tbcs_master_df.R")
+
+source("C:/Users/msara/OneDrive/Documents/NTU GHP/Thesis/Data/Outcome_tbcs-simulated/tbcs_tidy_data.R")
+
+source("C:/Users/msara/OneDrive/Documents/NTU GHP/Thesis/Data/Outcome_tbcs-simulated/tbcs_stat_analysis.R")
