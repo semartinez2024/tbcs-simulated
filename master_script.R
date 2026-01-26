1f6497acb35d5d6816808dc37cd89f27b0524f51
semartinez2024 <r12853008@ntu.edu.tw>
2026-01-26 16:57:14 -0500

    changed name of tidy data file and creates new r script for dealing with missing values

--- master_script.R
+++ master_script.R
@@ -1,5 +1,9 @@
+#make df
 source("C:/Users/msara/OneDrive/Documents/NTU GHP/Thesis/Data/Outcome_tbcs-simulated/tbcs_master_df.R")
 
-source("C:/Users/msara/OneDrive/Documents/NTU GHP/Thesis/Data/Outcome_tbcs-simulated/tbcs_tidy_data.R")
+#data cleaning
+source("C:/Users/msara/OneDrive/Documents/NTU GHP/Thesis/Data/Outcome_tbcs-simulated/tbcs_tidy_data_change_variable_type.R")
+source("C:/Users/msara/OneDrive/Documents/NTU GHP/Thesis/Data/Outcome_tbcs-simulated/tbcs_tidy_data_missing_values.R")
 
+#stats
 source("C:/Users/msara/OneDrive/Documents/NTU GHP/Thesis/Data/Outcome_tbcs-simulated/tbcs_stat_analysis.R")
