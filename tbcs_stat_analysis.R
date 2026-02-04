265dd42d9bb43708cb909c95985e508e39493eab
semartinez2024 <r12853008@ntu.edu.tw>
2026-02-03 20:49:15 -0500

    make stats table for probiotic at 6 months and 19 months

--- tbcs_stat_analysis.R
+++ tbcs_stat_analysis.R
@@ -11,9 +11,38 @@ browseURL("https://mayoverse.github.io/arsenal/articles/tableby.html")
 dim(combined_tbcs_data) #see rows and colums number
 str(combined_tbcs_data) #see structure of variables 
 
-table_descriptive_data <- tableby()
+#table 1: covariates and milestones, reference dag to choose and justify variables to include
+#make your own codebook/guidebook for Brian, so it's clear when he runs the code, can be in r or other doc type
 
-table_probiotic_6mo_side_milestones <- tableby(bifido_6mo ~ milestone_achievement 
+table_demographic_data <- tableby(bifido_6mo ~ combined_tbcs_data$infant_sex
+                                  + combined_tbcs_data$gestational_age
+                                  + combined_tbcs_data$birth_weight
+                                  + combined_tbcs_data$birth_type
+                                  , data = combined_tbcs_data)
+summary(table_demographic_data, text = TRUE)
+                       
+
+                         #maternal variables
+                        combined_tbcs_data$maternal_age 
+                        combined_tbcs_data$maternal_edu 
+                        combined_tbcs_data$exlcusively_breastfeeding_days 
+                        combined_tbcs_data$mother_smoking_status_before_pregnancy
+                        combined_tbcs_data$father_smoking_status_before_pregnancy
+                        combined_tbcs_data$mother_smoking_status_first_trimester 
+                        combined_tbcs_data$father_smoking_status_first_trimester 
+                        combined_tbcs_data$mother_smoking_status_second_trimester
+                        combined_tbcs_data$father_smoking_status_second_trimester
+                        combined_tbcs_data$mother_smoking_status_now 
+                        combined_tbcs_data$father_smoking_status_now 
+                        combined_tbcs_data$mother_alcohol_consumption_during_pregnancy 
+                        combined_tbcs_data$mother_alcohol_consumption_now_over_3x_per_week 
+                        combined_tbcs_data$average_monthly_income_past_year
+                        
+                        #environmental variables
+                        combined_tbcs_data$proximity_incinerator
+                        combined_tbcs_data$incense_burning_at_home
+
+table_probiotic_6mo <- tableby(bifido_6mo ~ milestone_achievement 
                                + milestone_walk_steadily 
                                + milestone_clapping
                                + milestone_scribble_with_pen
@@ -22,22 +51,24 @@ table_probiotic_6mo_side_milestones <- tableby(bifido_6mo ~ milestone_achievemen
                                + milestone_will_come_when_called
                                + milestone_drink_from_cup_with_both_hands
                                , data = combined_tbcs_data)
-table_probiotic_6mo_side_probiotics <- tableby(list(milestone_achievement, 
-                               milestone_walk_steadily, 
-                               milestone_clapping,
-                               milestone_scribble_with_pen,
-                               milestone_wave_goodbye,
-                               milestone_call_a_parent,
-                               milestone_will_come_when_called,
-                               milestone_drink_from_cup_with_both_hands) ~ bifido_6mo
-                               , data = combined_tbcs_data)
-summary(table_probiotic_6mo_side_milestones, text = TRUE) ##this table looks more concise but the explanatory variable is on the top (row) and the dependent variable is on the left side
 
-summary(table_probiotic_6mo_side_probiotics, text = TRUE)
+summary(table_probiotic_6mo, text = TRUE) ##explanatory variable is on the top (row) and the dependent variable is on the left side
 
-tableby.control() #test distrib of IV across levels of categorical variables
+table_probiotics_18mo <- tableby(bifido_18mo ~ milestone_achievement 
+                                 + milestone_walk_steadily 
+                                 + milestone_clapping
+                                 + milestone_scribble_with_pen
+                                 + milestone_wave_goodbye
+                                 + milestone_call_a_parent
+                                 + milestone_will_come_when_called
+                                 + milestone_drink_from_cup_with_both_hands
+                                 , data = combined_tbcs_data)
 
-#finalfit ----
+summary(table_probiotics_18mo, text = TRUE)
+
+tableby.control(table_probiotic_6mo) #test distrib of IV across levels of categorical variables
+
+#finalfit ----#ftable_probiotic_6moinalfit ----
 browseURL("https://finalfit.org/articles/all_tables_examples.html")
 explanatory = c("var1", "var2", "var3")
 dependent = "var4"
