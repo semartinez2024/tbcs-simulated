3b72a33203ec506c3fe505ae54b967a566ac8c95
semartinez2024 <r12853008@ntu.edu.tw>
2026-02-17 19:30:19 -0500

    renaming sections, completed na_if for dv, ran unique() for milestone variables

--- tbcs_tidy_data_missing_values_iv_dv.R
+++ tbcs_tidy_data_missing_values_iv_dv.R
@@ -1,39 +1,77 @@
 #Clean Data
 
-browseURL("https://r4ds.hadley.nz/missing-values.html")
-## deal with missing values ----
+browseURL("https://r4ds.hadley.nz/missing-values.html") #missing values chapter
+
+    na_if(#col name, value)
+    pivot_wider() > don't need for dv bc , values for variables are already single, there are no repeated measurements, the milestones were only measured at 18 mo not 6 mo
+    
+    pivot_longer() >> need for iv only, since its the only variable that was measured twice (repeated twice)
+    
+    orange 2 %>% 
+      pivot_longer(cols = 2:8,
+                  names_to = "AGE"
+                  values_to = "CIRC")
+    #values are explicitly missing, NOT implicityly missing
 
 #treat unknowns as missing in data, also some numerical values have been changed to character like breastfeeding_only_days, how should I approach this, first insert missing values?
 
-##independent variable ----
+## deal with independent variable missing values----
 
-#probiotic consumption 6-mo and 18-mo
-combined_tbcs_data$nutritional_supplement_consumption_6mo <- as.factor(combined_tbcs_data$nutritional_supplement_consumption_6mo)
-combined_tbcs_data$bifido_6mo <- as.factor(combined_tbcs_data$bifido_6mo)
-combined_tbcs_data$nutritional_supplement_consumption_18mo <- as.factor(combined_tbcs_data$nutritional_supplement_consumption_18mo)
-combined_tbcs_data$bifido_18mo <- as.factor(combined_tbcs_data$bifido_18mo)
+#probiotic consumption 6-mo
+combined_tbcs_data <- recode(combined_tbcs_data$nutritional_supplement_consumption_6mo, 9 = NA)  #0 = no, 1 = have, 9 = NA (refuse to answer, don't know, don't remember)
+bifido_6mo #0 = no, 1 = have, 9 = NA
+
+#probiotics 18 mo
+nutritional_supplement_consumption_18mo = D4 #0 = no, 1 = have, 8 = NA, 9 = unknown
+bifido_18mo = D4a4 #0 = no, 1 = have, 8 = NA, 9 = unknown
 
 ####create another variable to define as ever and never
 #####suggestion: do milestone in another file
 
-##dependent variable ----
+## deal with dependent variable missing values ----
 
 #developmental milestone variables >> managing data >> 90th percetile cutoff
-combined_tbcs_data$milestone_achievement <- as.factor(combined_tbcs_data$milestone_achievement)
-combined_tbcs_data$milestone_walk_steadily <- as.factor(combined_tbcs_data$milestone_walk_steadily)
-combined_tbcs_data$milestone_clapping <- as.factor(combined_tbcs_data$milestone_clapping)
-combined_tbcs_data$milestone_scribble_with_pen <- as.factor(combined_tbcs_data$milestone_scribble_with_pen)
-combined_tbcs_data$milestone_wave_goodbye <- as.factor(combined_tbcs_data$milestone_wave_goodbye)
-combined_tbcs_data$milestone_call_a_parent <- as.factor(combined_tbcs_data$milestone_call_a_parent)
-combined_tbcs_data$milestone_will_come_when_called <- as.factor(combined_tbcs_data$milestone_will_come_when_called)
-combined_tbcs_data$milestone_drink_from_cup_with_both_hands <- as.factor(combined_tbcs_data$milestone_drink_from_cup_with_both_hands)
+milestone_achievement = A6_1,	# 0 = not yet, 1 = meeting, 2 =  no idea
+milestone_walk_steadily = A6_2,	# 0 = not yet, 1 = meeting, 2 =  no idea
+milestone_clapping = A6_3,	# 0 = not yet, 1 = meeting, 2 =  no idea
+milestone_scribble_with_pen = A6_4,	# 0 = not yet, 1 = meeting, 2 =  no idea
+milestone_wave_goodbye = A6_5, # 0 = not yet, 1 = meeting, 2 =  no idea
+milestone_call_a_parent = A6_6,	# 0 = not yet, 1 = meeting, 2 =  no idea
+milestone_will_come_when_called = A6_7,	# 0 = not yet, 1 = meeting, 2 =  no idea
+milestone_drink_from_cup_with_both_hands = A6_8,	# 0 = not yet, 1 = meeting, 2 =  no idea
 
-#the month age of the milestone achievments might have an error i.e. no real values just NA, 8888, 9999
-combined_tbcs_data$month_age_of_milestone_achievement <- as.integer(combined_tbcs_data$month_age_of_milestone_achievement)	#90th percentile cutoff #only presents 9999 = have no idea 8888 = not applicable
-combined_tbcs_data$month_age_milestone_walk_steadily <- as.integer(combined_tbcs_data$month_age_milestone_walk_steadily)
-combined_tbcs_data$month_age_milestone_clapping <- as.integer(combined_tbcs_data$month_age_milestone_clapping)
-combined_tbcs_data$month_age_milestone_scribble_with_pen <- as.integer(combined_tbcs_data$month_age_milestone_scribble_with_pen)
-combined_tbcs_data$month_age_milestone_wave_goodbye <- as.integer(combined_tbcs_data$month_age_milestone_wave_goodbye)	
-combined_tbcs_data$month_age_milestone_call_a_parent <- as.integer(combined_tbcs_data$month_age_milestone_call_a_parent)
-combined_tbcs_data$month_age_milestone_will_come_when_called <- as.integer(combined_tbcs_data$month_age_milestone_will_come_when_called)
-combined_tbcs_data$month_age_milestone_drink_from_cup_with_both_hands <- as.integer(combined_tbcs_data$month_age_milestone_drink_from_cup_with_both_hands)
+na_if(combined_tbcs_data$month_age_of_milestone_achievement, 8888)
+na_if(combined_tbcs_data$month_age_of_milestone_achievement, 9999)
+na_if(combined_tbcs_data$month_age_milestone_walk_steadily, 8888)
+na_if(combined_tbcs_data$month_age_milestone_walk_steadily, 9999)
+na_if(combined_tbcs_data$month_age_milestone_clapping, 8888)
+na_if(combined_tbcs_data$month_age_milestone_clapping, 9999)
+na_if(combined_tbcs_data$month_age_milestone_scribble_with_pen, 8888)
+na_if(combined_tbcs_data$month_age_milestone_scribble_with_pen, 9999)
+na_if(combined_tbcs_data$month_age_milestone_wave_goodbye, 8888)
+na_if(combined_tbcs_data$month_age_milestone_wave_goodbye, 9999)
+na_if(combined_tbcs_data$month_age_milestone_call_a_parent, 8888)
+na_if(combined_tbcs_data$month_age_milestone_call_a_parent, 9999)
+na_if(combined_tbcs_data$month_age_milestone_will_come_when_called, 8888)
+na_if(combined_tbcs_data$month_age_milestone_will_come_when_called, 9999)
+na_if(combined_tbcs_data$month_age_milestone_drink_from_cup_with_both_hands, 8888)
+na_if(combined_tbcs_data$month_age_milestone_drink_from_cup_with_both_hands, 9999) #16 lines for 8 milestones x2
+
+month_age_of_milestone_achievement = A6a_1,	# whole number, 8888 = not applicable, 9999 = have no idea
+month_age_milestone_walk_steadily = A6a_2,	# whole number, 8888 = not applicable, 9999 = have no idea
+month_age_milestone_clapping = A6a_3,	# whole number, 8888 = not applicable, 9999 = have no idea
+month_age_milestone_scribble_with_pen = A6a_4,	# whole number, 8888 = not applicable, 9999 = have no idea
+month_age_milestone_wave_goodbye = A6a_5,	# whole number, 8888 = not applicable, 9999 = have no idea
+month_age_milestone_call_a_parent = A6a_6,	# whole number, 8888 = not applicable, 9999 = have no idea
+month_age_milestone_will_come_when_called = A6a_7,	# whole number, 8888 = not applicable, 9999 = have no idea
+month_age_milestone_drink_from_cup_with_both_hands = A6a_8,# whole number, 8888 = not applicable, 9999 = have no idea
+
+unique(combined_tbcs_data$month_age_of_milestone_achievement)
+unique(combined_tbcs_data$month_age_milestone_walk_steadily)
+unique(combined_tbcs_data$month_age_milestone_clapping)
+unique(combined_tbcs_data$month_age_milestone_scribble_with_pen)
+unique(combined_tbcs_data$month_age_milestone_wave_goodbye)
+unique(combined_tbcs_data$month_age_milestone_call_a_parent)
+unique(combined_tbcs_data$month_age_milestone_will_come_when_called)
+unique(combined_tbcs_data$month_age_milestone_drink_from_cup_with_both_hands)
+
