c15a6c72b4e7e486130ac735da2bb0b9d981cfb2
semartinez2024 <r12853008@ntu.edu.tw>
2026-04-04 16:23:40 -0400

    fixed ever and never probiotics, removed NEVER last three lines because NEVER just needs to be one line and can be on the same line as EVER

--- tbcs_tidy_data_missing_values_iv.R
+++ tbcs_tidy_data_missing_values_iv.R
@@ -2,13 +2,6 @@
 
 browseURL("https://r4ds.hadley.nz/missing-values.html") #missing values chapter
 
-#na_if(#col name, value)
- 
-    #pivot_longer() >> need for iv only, since its the only variable that was measured twice (repeated twice)
-    
-    #orange 2 %>% pivot_longer(cols = 2:8, names_to = "AGE" values_to = "CIRC")
-    #values are explicitly missing, NOT implicityly missing
-
 ## deal with independent variable missing values----
 
 #probiotic consumption 6-mo
@@ -19,14 +12,12 @@ na_if(combined_tbcs_data$bifido_6mo, 9) #0 = no, 1 = have, 9 = NA
 na_if(combined_tbcs_data$nutritional_supplement_consumption_18mo, 9) #0 = no, 1 = have, 8 = NA, 9 = unknown
 na_if(combined_tbcs_data$bifido_18mo, 9) #0 = no, 1 = have, 8 = NA, 9 = unknown
 
-####create another variable to define as ever and never
-probiotic_ever <- combined_tbcs_data %>% case_when((bifido_6mo == 1)&(bifido_18mo== 1) ~ "ever",
-                                                   case_when(bifido_6mo == 0)&(bifido_18mo== 1) ~ "ever",
-                                                             case_when(bifido_6mo == 1)&(bifido_18mo== 0) ~ "ever",
-                                                                       case_when(bifido_6mo == 1)&(bifido_18mo== 9) ~ "ever",
-                                                                                 case_when(bifido_6mo == 9)&(bifido_18mo== 1) ~ "ever")
-
-probiotic_never <- combined_tbcs_data %>% case_when((bifido_6mo == 0)&(bifido_18mo== 0) ~ "never",
-                                                    case_when(bifido_6mo == 9)&(18 months== 9) ~ "never",
-                                                            case_when(bifido_6mo == 0)&(18 months== 9) ~ "never",
-                                                                    case_when(bifido_6mo == 9)&(18 months== 0) ~ "never")                                                         
+####create another variable to define as ever and never----
+combined_tbcs_data <- combined_tbcs_data %>% 
+                          mutate (probiotic = case_when((bifido_6mo == 1)&(bifido_18mo == 1) ~ "ever",
+                                                          (bifido_6mo == 0)&(bifido_18mo == 1) ~ "ever",
+                                                          (bifido_6mo == 1)&(bifido_18mo == 0) ~ "ever",
+                                                          (bifido_6mo == 1)&(bifido_18mo == 9) ~ "ever",
+                                                          (bifido_6mo == 9)&(bifido_18mo == 1) ~ "ever",
+                                                          (bifido_6mo == 0)&(bifido_18mo == 0) ~ "never"))
+                                                                                     
