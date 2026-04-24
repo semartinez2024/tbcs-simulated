4d5dd3f75772d500ab4e49c5ccf93dc19081a282
semartinez2024 <r12853008@ntu.edu.tw>
2026-04-24 00:20:49 -0400

    going through response values for covariates, will also use this outline for another script dealing directly with values (not just NAs)

--- tbcs_tidy_data_missing_values_demographic_covariates_confounders.R
+++ tbcs_tidy_data_missing_values_demographic_covariates_confounders.R
@@ -1,33 +1,68 @@
 #Clean Data
 
+#treat unknowns as missing in data, also some numerical values have been changed to character like breastfeeding_only_days, how should I approach this, first insert missing values?
+na_if(combined_tbcs_data$nutritional_supplement_consumption_6mo, 9)
+
 ##demographic variables ----
-combined_tbcs_data$participant_identification <- as.character(combined_tbcs_data$participant_identification)
-combined_tbcs_data$infant_sex <- as.factor(combined_tbcs_data$infant_sex)
+  #participant identification doesn't need NA, that was already fixed when using join() in df script
+  #2= female, 1 = male, NA  
+  na_if(combined_tbcs_data$infant_sex,
 
 
 ##covariates/confounders ----
 
 #infant variables
-combined_tbcs_data$gestational_age <- as.factor(combined_tbcs_data$gestational_age)
-combined_tbcs_data$birth_weight <- as.factor(combined_tbcs_data$birth_weight)
-combined_tbcs_data$birth_type <-as.factor(combined_tbcs_data$birth_type)
-
+#3 = 37wk+, 2 = 28-37wk, 1 = less than 28wk, NA
+na_if(combined_tbcs_data$gestational_age, )
+# 2. 1500-2499 grams 3. 2500-3499 grams 4. More than 3500 grams, NA
+na_if(combined_tbcs_data$birth_weight, 
+                              #1. Single pregnancy 2. Twin pregnancy or more, NA, 9      
+                              na_if(combined_tbcs_data$birth_type,
+                                    
+     
 #maternal variables
-combined_tbcs_data$maternal_age <- as.integer(combined_tbcs_data$maternal_age)
-combined_tbcs_data$maternal_edu <- as.factor(combined_tbcs_data$maternal_edu)
-combined_tbcs_data$exlcusively_breastfeeding_days <- as.integer(combined_tbcs_data$exlcusively_breastfeeding_days) #treat unknowns as missing in data, also some numerical values have been changed to character like breastfeeding_only_days, how should I approach this, first insert missing values?
-combined_tbcs_data$mother_smoking_status_before_pregnancy <- as.factor(combined_tbcs_data$mother_smoking_status_before_pregnancy)
-combined_tbcs_data$father_smoking_status_before_pregnancy <- as.factor(combined_tbcs_data$father_smoking_status_before_pregnancy)
-combined_tbcs_data$mother_smoking_status_first_trimester <- as.factor(combined_tbcs_data$mother_smoking_status_first_trimester)
-combined_tbcs_data$father_smoking_status_first_trimester <- as.factor(combined_tbcs_data$father_smoking_status_first_trimester)
-combined_tbcs_data$mother_smoking_status_second_trimester <- as.factor(combined_tbcs_data$mother_smoking_status_second_trimester)
-combined_tbcs_data$father_smoking_status_second_trimester <- as.factor(combined_tbcs_data$father_smoking_status_second_trimester)
-combined_tbcs_data$mother_smoking_status_now <- as.factor(combined_tbcs_data$mother_smoking_status_now)
-combined_tbcs_data$father_smoking_status_now <- as.factor(combined_tbcs_data$father_smoking_status_now)
-combined_tbcs_data$mother_alcohol_consumption_during_pregnancy <- as.factor(combined_tbcs_data$mother_alcohol_consumption_during_pregnancy)
-combined_tbcs_data$mother_alcohol_consumption_now_over_3x_per_week <- as.factor(combined_tbcs_data$mother_alcohol_consumption_now_over_3x_per_week)
-combined_tbcs_data$average_monthly_income_past_year <- as.factor(combined_tbcs_data$average_monthly_income_past_year)
-
+#integer or NA
+na_if(combined_tbcs_data$maternal_age,
+#1. Junior high school and below 2. Senior high school 3. College and above, NA
+na_if(combined_tbcs_data$maternal_edu,
+#integer or NA
+na_if(combined_tbcs_data$exlcusively_breastfeeding_days, 
+                            #0. No1.have9.NA
+                            na_if(combined_tbcs_data$mother_smoking_status_before_pregnancy,
+                            #0. No1.have9.NA
+                            na_if(combined_tbcs_data$father_smoking_status_before_pregnancy,
+                            #0. No1.have9.NA
+                            na_if(combined_tbcs_data$mother_smoking_status_first_trimester,
+                            #0. No1.have9.NA
+                            na_if(combined_tbcs_data$father_smoking_status_first_trimester,
+na_if(combined_tbcs_data$mother_smoking_status_second_trimester,
+na_if(combined_tbcs_data$father_smoking_status_second_trimester,
+na_if(combined_tbcs_data$mother_smoking_status_now,
+na_if(combined_tbcs_data$father_smoking_status_now,
+na_if(combined_tbcs_data$mother_alcohol_consumption_during_pregnancy,
+na_if(combined_tbcs_data$mother_alcohol_consumption_now_over_3x_per_week,
+na_if(combined_tbcs_data$average_monthly_income_past_year,
+      
+      #parental factors
+      maternal_age = Mage, #integer
+      maternal_edu = Medu, #factor
+      exlcusively_breastfeeding_days = C2ad, #integer
+      mother_smoking_status_before_pregnancy = H1aa,	#any Y = Y
+      father_smoking_status_before_pregnancy = H1ba,	
+      mother_smoking_status_first_trimester = H1ab,	
+      father_smoking_status_first_trimester = H1bb,  ##8 is mose likely error, you should consider NA
+      mother_smoking_status_second_trimester = H1ac,
+      father_smoking_status_second_trimester = H1bc,	
+      mother_smoking_status_now = H1ad,
+      father_smoking_status_now = H1bd,
+      mother_alcohol_consumption_during_pregnancy = H2_b, #1 = Y . combine with now over 3x wk
+      mother_alcohol_consumption_now_over_3x_per_week = H2a_c,	#1 = Y
+      average_monthly_income_past_year = H13,
+      
 #environmental variables
-combined_tbcs_data$proximity_incinerator <- as.factor(combined_tbcs_data$proximity_incinerator)
-combined_tbcs_data$incense_burning_at_home <- as.factor(combined_tbcs_data$incense_burning_at_home)
+na_if(combined_tbcs_data$proximity_incinerator,
+na_if(combined_tbcs_data$incense_burning_at_home,
+      
+      #environmental factors
+      proximity_incinerator = K1_1,
+      incense_burning_at_home = K3,
