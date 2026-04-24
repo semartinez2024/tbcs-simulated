bd36d0f7f28acdc03b115365486b1df69940701e
semartinez2024 <r12853008@ntu.edu.tw>
2026-04-24 00:19:39 -0400

    script for missing dv values are done

--- master_script.R
+++ master_script.R
@@ -5,15 +5,15 @@
   
   #data cleaning
   source("C:/Users/msara/OneDrive/DocumentsNTU GHP/Thesis/Data/Outcome_tbcs-simulated/tbcs_tidy_data_missing_values_iv.R") #this deals with NA
+  source("C:/Users/msara/OneDrive/DocumentsNTU GHP/Thesis/Data/Outcome_tbcs-simulated/tbcs_tidy_data_missing_values_dv.R")  
   source("C:/Users/msara/OneDrive/Documents/NTU GHP/Thesis/Data/Outcome_tbcs-simulated/tbcs_tidy_data_variable_type.R") #do after dealing with NA so NAs are preserved
-  
+    
   #stats
 
 
 #In-Progress----
   
   #data cleaning
-  source("C:/Users/msara/OneDrive/DocumentsNTU GHP/Thesis/Data/Outcome_tbcs-simulated/tbcs_tidy_data_missing_values_dv.R")
   source("C:/Users/msara/OneDrive/Documents/NTU GHP/Thesis/Data/Outcome_tbcs-simulated/tbcs_tidy_data_values_dv.R")
   source("C:/Users/msara/OneDrive/Documents/NTU GHP/Thesis/Data/Outcome_tbcs-simulated/tbcs_tidy_data_missing_values_demographic_covariates_confounders.R")
   
