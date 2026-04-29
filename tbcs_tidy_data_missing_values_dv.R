aa58127dc2c720dc0e6abe22be9f4bd912c5118f
semartinez2024 <r12853008@ntu.edu.tw>
2026-04-28 20:31:05 -0400

    wrapped factor responses in quotations to treat them like character string so I can convert to NA

--- tbcs_tidy_data_missing_values_dv.R
+++ tbcs_tidy_data_missing_values_dv.R
@@ -6,14 +6,14 @@ browseURL("https://r4ds.hadley.nz/missing-values.html") #missing values chapter
 
 #responses for variables below 0 = not yet, 1 = meeting, 2 = have no idea
 
-na_if(combined_tbcs_data$milestone_achievement, 2)
-na_if(combined_tbcs_data$milestone_walk_steadily, 2)
-na_if(combined_tbcs_data$milestone_clapping, 2)
-na_if(combined_tbcs_data$milestone_scribble_with_pen, 2)
-na_if(combined_tbcs_data$milestone_wave_goodbye, 2)
-na_if(combined_tbcs_data$milestone_call_a_parent, 2)
-na_if(combined_tbcs_data$milestone_will_come_when_called, 2)
-na_if(combined_tbcs_data$milestone_drink_from_cup_with_both_hands, 2)
+na_if(combined_tbcs_data$milestone_achievement, "2")
+na_if(combined_tbcs_data$milestone_walk_steadily, "2")
+na_if(combined_tbcs_data$milestone_clapping, "2")
+na_if(combined_tbcs_data$milestone_scribble_with_pen, "2")
+na_if(combined_tbcs_data$milestone_wave_goodbye, "2")
+na_if(combined_tbcs_data$milestone_call_a_parent, "2")
+na_if(combined_tbcs_data$milestone_will_come_when_called, "2")
+na_if(combined_tbcs_data$milestone_drink_from_cup_with_both_hands, "2")
 
 #responses for variables below whole number, 8888 = not applicable, 9999 = have no idea
 
