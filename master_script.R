45fd3fce0be3900fa4e9fb53a3898478a609634f
semartinez2024 <r12853008@ntu.edu.tw>
2026-04-29 22:36:52 -0400

    moved script for changing variable type up

--- master_script.R
+++ master_script.R
@@ -4,9 +4,9 @@
   source("C:/Users/msara/OneDrive/Documents/NTU GHP/Thesis/Data/Outcome_tbcs-simulated/tbcs_master_df.R")
   
   #data cleaning
+  source("C:/Users/msara/OneDrive/Documents/NTU GHP/Thesis/Data/Outcome_tbcs-simulated/tbcs_tidy_data_variable_type.R") #do after dealing with NA so NAs are preserved  
   source("C:/Users/msara/OneDrive/Documents/NTU GHP/Thesis/Data/Outcome_tbcs-simulated/tbcs_tidy_data_missing_values_iv.R") #data_missing_values scripts all deal with NAs, the iv script includes codes for missing values and values for probiotic
   source("C:/Users/msara/OneDrive/Documents/NTU GHP/Thesis/Data/Outcome_tbcs-simulated/tbcs_tidy_data_missing_values_dv.R")  
-  source("C:/Users/msara/OneDrive/Documents/NTU GHP/Thesis/Data/Outcome_tbcs-simulated/tbcs_tidy_data_variable_type.R") #do after dealing with NA so NAs are preserved
   source("C:/Users/msara/OneDrive/Documents/NTU GHP/Thesis/Data/Outcome_tbcs-simulated/tbcs_tidy_data_missing_values_demographic_covariates_confounders.R")
   
   #stats
