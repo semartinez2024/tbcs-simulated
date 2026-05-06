d40fdc70cef2430e0d265749d72295e3835e01cc
semartinez2024 <r12853008@ntu.edu.tw>
2026-05-05 22:43:41 -0400

    found 90th percentiles for all milestone variables (that have month as response)

--- tbcs_tidy_data_values_dv.R
+++ tbcs_tidy_data_values_dv.R
@@ -1,10 +1,8 @@
 #Clean Data
 
-## rename dependent variable 'meeting' and 'not yet' values ----
-
 # missing values have been changed to NA, so now I need to deal with integars by grouping into above and below 90th percentile
 # responses for variables below are > whole number, 8888 = not applicable, 9999 = have no idea
-quantile(combined_tbcs_data$month_age_of_milestone_achievement)
+combined_tbcs_data$month_age_of_milestone_achievement
 combined_tbcs_data$month_age_milestone_walk_steadily
 combined_tbcs_data$month_age_milestone_clapping
 combined_tbcs_data$month_age_milestone_scribble_with_pen
@@ -14,6 +12,15 @@ combined_tbcs_data$month_age_milestone_will_come_when_called
 combined_tbcs_data$month_age_milestone_drink_from_cup_with_both_hands
 ## checking histogram fordistribution of month ages for the above
 
-# find 90 prcentile >> then mutate to new variable (above and below 90th)
-ggplot(combined_tbcs_data, aes(x = month_age_of_milestone_achievement)) +
-  geom_histogram(binwidth = 0.5)
+#find 90 prcentile ----
+quantile(combined_tbcs_data$month_age_of_milestone_achievement, 0.9, na.rm = TRUE) #14
+quantile(combined_tbcs_data$month_age_milestone_walk_steadily, 0.9, na.rm = TRUE) #18
+quantile(combined_tbcs_data$month_age_milestone_clapping, 0.9, na.rm = TRUE) #13
+quantile(combined_tbcs_data$month_age_milestone_scribble_with_pen, 0.9, na.rm = TRUE) #19
+quantile(combined_tbcs_data$month_age_milestone_wave_goodbye, 0.9, na.rm = TRUE) #14
+quantile(combined_tbcs_data$month_age_milestone_call_a_parent, 0.9, na.rm = TRUE) #14
+quantile(combined_tbcs_data$month_age_milestone_will_come_when_called, 0.9, na.rm = TRUE) #14
+quantile(combined_tbcs_data$month_age_milestone_drink_from_cup_with_both_hands, 0.9, na.rm = TRUE) #17
+
+#mutate to new variable (above and below 90th percentile)
+#rename dependent variable 'meeting' and 'not yet' values ----
