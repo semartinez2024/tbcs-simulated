bdda136a87306391e1b2f4c7aedc4e719c5fb3df
semartinez2024 <r12853008@ntu.edu.tw>
2026-07-21 23:27:59 -0400

    changing dv values into binary 0/1

--- tbcs_tidy_data_values_dv.R
+++ tbcs_tidy_data_values_dv.R
@@ -10,7 +10,8 @@ combined_tbcs_data$month_age_milestone_wave_goodbye
 combined_tbcs_data$month_age_milestone_call_a_parent
 combined_tbcs_data$month_age_milestone_will_come_when_called
 combined_tbcs_data$month_age_milestone_drink_from_cup_with_both_hands
-## checking histogram fordistribution of month ages for the above
+
+## checking histogram for distribution of month ages for the above
 
 #find 90 prcentile ----
 general_milestone_90 <- quantile(combined_tbcs_data$month_age_of_milestone_achievement, 0.9, na.rm = TRUE)
@@ -21,6 +22,49 @@ wave_milestone_90 <- quantile(combined_tbcs_data$month_age_milestone_wave_goodby
 call_milestone_90 <- quantile(combined_tbcs_data$month_age_milestone_call_a_parent, 0.9, na.rm = TRUE)
 come_milestone_90 <- quantile(combined_tbcs_data$month_age_milestone_will_come_when_called, 0.9, na.rm = TRUE)
 drink_milestone_90 <- quantile(combined_tbcs_data$month_age_milestone_drink_from_cup_with_both_hands, 0.9, na.rm = TRUE)
+
+#change integer values from numeric to binary (i.e 14 to 0/1) ----
+## 0 = NOT MET, 1 = MET
+combined_tbcs_data <- combined_tbcs_data %>%
+  mutate(month_age_of_milestone_achievement = case_when(month_age_of_milestone_achievement >= general_milestone_90 ~ "0",
+                                                        month_age_of_milestone_achievement <= general_milestone_90 ~ "1"))
+combined_tbcs_data <- combined_tbcs_data %>%
+  mutate(month_age_milestone_walk_steadily = case_when(month_age_milestone_walk_steadily >= walk_milestone_90 ~ "0",
+                                                       month_age_milestone_walk_steadily <= walk_milestone_90 ~ "1"))
+
+
+
+
+---------------------------------------------------------------------------------------------------------
+combined_tbcs_data <- combined_tbcs_data %>%
+  mutate(month_age_milestone_walk_steadily = case_when(month_age_milestone_walk_steadily >= walk_milestone_90 ~ "0",
+                                                       month_age_milestone_walk_steadily <= walk_milestone_90 ~ "1"))
+
+combined_tbcs_data <- combined_tbcs_data %>%
+  mutate(month_age_milestone_walk_steadily = case_when(month_age_milestone_walk_steadily >= walk_milestone_90 ~ "0",
+                                                       month_age_milestone_walk_steadily <= walk_milestone_90 ~ "1"))
+
+combined_tbcs_data <- combined_tbcs_data %>%
+  mutate(month_age_milestone_walk_steadily = case_when(month_age_milestone_walk_steadily >= walk_milestone_90 ~ "0",
+                                                       month_age_milestone_walk_steadily <= walk_milestone_90 ~ "1"))
+
+combined_tbcs_data <- combined_tbcs_data %>%
+  mutate(month_age_milestone_walk_steadily = case_when(month_age_milestone_walk_steadily >= walk_milestone_90 ~ "0",
+                                                       month_age_milestone_walk_steadily <= walk_milestone_90 ~ "1"))
+
+combined_tbcs_data <- combined_tbcs_data %>%
+  mutate(month_age_milestone_walk_steadily = case_when(month_age_milestone_walk_steadily >= walk_milestone_90 ~ "0",
+                                                       month_age_milestone_walk_steadily <= walk_milestone_90 ~ "1"))
+
+combined_tbcs_data <- combined_tbcs_data %>%
+  mutate(month_age_milestone_walk_steadily = case_when(month_age_milestone_walk_steadily >= walk_milestone_90 ~ "0",
+                                                       month_age_milestone_walk_steadily <= walk_milestone_90 ~ "1"))
+
+combined_tbcs_data <- combined_tbcs_data %>%
+  mutate(month_age_milestone_walk_steadily = case_when(month_age_milestone_walk_steadily >= walk_milestone_90 ~ "0",
+                                                       month_age_milestone_walk_steadily <= walk_milestone_90 ~ "1"))
+
+
 
 #mutate to new variable (below 90th percentile is MET, above 90th percentile is NOT MET i.e., delayed) ----
 combined_tbcs_data <- combined_tbcs_data %>% 
