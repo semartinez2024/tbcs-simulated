6b3485554f3cbc47dddca5f79565032889ac0537
semartinez2024 <r12853008@ntu.edu.tw>
2026-07-21 21:19:22 -0400

    remove values for 90th percentile, instead will use new object for next part of code

--- tbcs_tidy_data_values_dv.R
+++ tbcs_tidy_data_values_dv.R
@@ -13,18 +13,22 @@ combined_tbcs_data$month_age_milestone_drink_from_cup_with_both_hands
 ## checking histogram fordistribution of month ages for the above
 
 #find 90 prcentile ----
-quantile(combined_tbcs_data$month_age_of_milestone_achievement, 0.9, na.rm = TRUE) #14
-quantile(combined_tbcs_data$month_age_milestone_walk_steadily, 0.9, na.rm = TRUE) #18
-quantile(combined_tbcs_data$month_age_milestone_clapping, 0.9, na.rm = TRUE) #13
-quantile(combined_tbcs_data$month_age_milestone_scribble_with_pen, 0.9, na.rm = TRUE) #19
-quantile(combined_tbcs_data$month_age_milestone_wave_goodbye, 0.9, na.rm = TRUE) #14
-quantile(combined_tbcs_data$month_age_milestone_call_a_parent, 0.9, na.rm = TRUE) #14
-quantile(combined_tbcs_data$month_age_milestone_will_come_when_called, 0.9, na.rm = TRUE) #14
-quantile(combined_tbcs_data$month_age_milestone_drink_from_cup_with_both_hands, 0.9, na.rm = TRUE) #17
+general_milestone_90 <- quantile(combined_tbcs_data$month_age_of_milestone_achievement, 0.9, na.rm = TRUE)
+walk_milestone_90 <- quantile(combined_tbcs_data$month_age_milestone_walk_steadily, 0.9, na.rm = TRUE)
+clap_milestone_90 <- quantile(combined_tbcs_data$month_age_milestone_clapping, 0.9, na.rm = TRUE)
+scribble_milestone_90 <- quantile(combined_tbcs_data$month_age_milestone_scribble_with_pen, 0.9, na.rm = TRUE)
+wave_milestone_90 <- quantile(combined_tbcs_data$month_age_milestone_wave_goodbye, 0.9, na.rm = TRUE)
+call_milestone_90 <- quantile(combined_tbcs_data$month_age_milestone_call_a_parent, 0.9, na.rm = TRUE)
+come_milestone_90 <- quantile(combined_tbcs_data$month_age_milestone_will_come_when_called, 0.9, na.rm = TRUE)
+drink_milestone_90 <- quantile(combined_tbcs_data$month_age_milestone_drink_from_cup_with_both_hands, 0.9, na.rm = TRUE)
 
 #mutate to new variable (below 90th percentile is MET, above 90th percentile is NOT MET i.e., delayed) ----
 combined_tbcs_data <- combined_tbcs_data %>% 
-  mutate(under_90 = if_else(month_age_of_milestone_achievement < quantile(month_age_of_milestone_achievement, 0.9), "MET" , "NOT MET"))
+  mutate(percentile_90_milestone_achievement = quantile(month_age_of_milestone_achievement, probs = 0.9, na.rm = TRUE))
+
+## compare every values to the 90th percentile values, above it is not met, below it is met, then 
+view(combined_tbcs_data$month_age_of_milestone_achievement)
+
 combined_tbcs_data$month_age_milestone_walk_steadily <-
 combined_tbcs_data$month_age_milestone_clapping <-
 combined_tbcs_data$month_age_milestone_scribble_with_pen <-
