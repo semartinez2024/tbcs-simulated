d6bd68792c60e7e28c0daab8650d89cfe6a9b346
semartinez2024 <r12853008@ntu.edu.tw>
2026-07-22 22:18:47 -0400

    removed unnecessaary lines, finished mutating outcomes variables into binaries 0 = not met and 1 = met

--- tbcs_tidy_data_values_dv.R
+++ tbcs_tidy_data_values_dv.R
@@ -1,16 +1,5 @@
 #Clean Data
 
-# missing values have been changed to NA, so now I need to deal with integars by grouping into above and below 90th percentile
-# responses for variables below are > whole number, 8888 = not applicable, 9999 = have no idea
-combined_tbcs_data$month_age_of_milestone_achievement
-combined_tbcs_data$month_age_milestone_walk_steadily
-combined_tbcs_data$month_age_milestone_clapping
-combined_tbcs_data$month_age_milestone_scribble_with_pen
-combined_tbcs_data$month_age_milestone_wave_goodbye
-combined_tbcs_data$month_age_milestone_call_a_parent
-combined_tbcs_data$month_age_milestone_will_come_when_called
-combined_tbcs_data$month_age_milestone_drink_from_cup_with_both_hands
-
 ## checking histogram for distribution of month ages for the above
 
 #find 90 prcentile ----
@@ -31,55 +20,26 @@ combined_tbcs_data <- combined_tbcs_data %>%
 combined_tbcs_data <- combined_tbcs_data %>%
   mutate(month_age_milestone_walk_steadily = case_when(month_age_milestone_walk_steadily >= walk_milestone_90 ~ "0",
                                                        month_age_milestone_walk_steadily <= walk_milestone_90 ~ "1"))
-
-
-
-
----------------------------------------------------------------------------------------------------------
 combined_tbcs_data <- combined_tbcs_data %>%
-  mutate(month_age_milestone_walk_steadily = case_when(month_age_milestone_walk_steadily >= walk_milestone_90 ~ "0",
-                                                       month_age_milestone_walk_steadily <= walk_milestone_90 ~ "1"))
-
+  mutate(month_age_milestone_clapping = case_when(month_age_milestone_clapping >= clap_milestone_90 ~ "0",
+                                                       month_age_milestone_clapping <= clap_milestone_90 ~ "1"))
 combined_tbcs_data <- combined_tbcs_data %>%
-  mutate(month_age_milestone_walk_steadily = case_when(month_age_milestone_walk_steadily >= walk_milestone_90 ~ "0",
-                                                       month_age_milestone_walk_steadily <= walk_milestone_90 ~ "1"))
-
+  mutate(month_age_milestone_scribble_with_pen = case_when(month_age_milestone_scribble_with_pen >= scribble_milestone_90 ~ "0",
+                                                        month_age_milestone_scribble_with_pen <= scribble_milestone_90 ~ "1"))
 combined_tbcs_data <- combined_tbcs_data %>%
-  mutate(month_age_milestone_walk_steadily = case_when(month_age_milestone_walk_steadily >= walk_milestone_90 ~ "0",
-                                                       month_age_milestone_walk_steadily <= walk_milestone_90 ~ "1"))
-
+  mutate(month_age_milestone_wave_goodbye = case_when(month_age_milestone_wave_goodbye >= wave_milestone_90 ~ "0",
+                                                      month_age_milestone_wave_goodbye <= wave_milestone_90 ~ "1"))
 combined_tbcs_data <- combined_tbcs_data %>%
-  mutate(month_age_milestone_walk_steadily = case_when(month_age_milestone_walk_steadily >= walk_milestone_90 ~ "0",
-                                                       month_age_milestone_walk_steadily <= walk_milestone_90 ~ "1"))
-
+  mutate(month_age_milestone_call_a_parent = case_when(month_age_milestone_call_a_parent >= call_milestone_90 ~ "0",
+                                                       month_age_milestone_call_a_parent <= call_milestone_90 ~ "1"))
 combined_tbcs_data <- combined_tbcs_data %>%
-  mutate(month_age_milestone_walk_steadily = case_when(month_age_milestone_walk_steadily >= walk_milestone_90 ~ "0",
-                                                       month_age_milestone_walk_steadily <= walk_milestone_90 ~ "1"))
-
+  mutate(month_age_milestone_will_come_when_called = case_when(month_age_milestone_will_come_when_called >= come_milestone_90 ~ "0",
+                                                       month_age_milestone_will_come_when_called <= come_milestone_90 ~ "1"))
 combined_tbcs_data <- combined_tbcs_data %>%
-  mutate(month_age_milestone_walk_steadily = case_when(month_age_milestone_walk_steadily >= walk_milestone_90 ~ "0",
-                                                       month_age_milestone_walk_steadily <= walk_milestone_90 ~ "1"))
-
-combined_tbcs_data <- combined_tbcs_data %>%
-  mutate(month_age_milestone_walk_steadily = case_when(month_age_milestone_walk_steadily >= walk_milestone_90 ~ "0",
-                                                       month_age_milestone_walk_steadily <= walk_milestone_90 ~ "1"))
+  mutate(month_age_milestone_drink_from_cup_with_both_hands = case_when(month_age_milestone_drink_from_cup_with_both_hands >= drink_milestone_90 ~ "0",
+                                                       month_age_milestone_drink_from_cup_with_both_hands <= drink_milestone_90 ~ "1"))
 
 
-
-#mutate to new variable (below 90th percentile is MET, above 90th percentile is NOT MET i.e., delayed) ----
-combined_tbcs_data <- combined_tbcs_data %>% 
-  mutate(percentile_90_milestone_achievement = quantile(month_age_of_milestone_achievement, probs = 0.9, na.rm = TRUE))
-
-## compare every values to the 90th percentile values, above it is not met, below it is met, then 
-view(combined_tbcs_data$month_age_of_milestone_achievement)
-
-combined_tbcs_data$month_age_milestone_walk_steadily <-
-combined_tbcs_data$month_age_milestone_clapping <-
-combined_tbcs_data$month_age_milestone_scribble_with_pen <-
-combined_tbcs_data$month_age_milestone_wave_goodbye <-
-combined_tbcs_data$month_age_milestone_call_a_parent <-
-combined_tbcs_data$month_age_milestone_will_come_when_called <-
-combined_tbcs_data$month_age_milestone_drink_from_cup_with_both_hands <-
 
 combined_tbcs_data <- combined_tbcs_data %>% 
   mutate (milestone = case_when((bifido_6mo == 1)&(bifido_18mo == 1) ~ "ever",
